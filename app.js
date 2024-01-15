e43d2c0b75a92264b047ccb526b100fdb5adf0cb
AndreyMazer <pyshnograyandrey@yandex.ru>
2024-01-15 20:35:15 +0300

    1501

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(errLog);
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
-  res.status(err.statusCode).send({
+  res.status(statusCode).send({
     message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
   });
 
